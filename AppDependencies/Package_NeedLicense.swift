618c416d545e5f884dcb4e0d7f485fcb408970ce
Shangxin Guo <int123c@gmail.com>
2020-11-16 22:55:48 +0800

    Fix package file

--- AppDependencies/Package_NeedLicense.swift
+++ AppDependencies/Package_NeedLicense.swift
@@ -34,6 +34,10 @@ let package = Package(
             url: "https://github.com/CombineCommunity/CombineExt",
             .upToNextMajor(from: "1.2.0")
         ),
+        .package(
+            url: "https://github.com/sparkle-project/Sparkle",
+            .branch("master")
+        ),
     ],
     targets: [
         .target(
@@ -43,6 +47,7 @@ let package = Package(
                 "CGEventOverride",
                 "CombineExt",
                 "KeychainAccess",
+                "Sparkle",
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
             ]
         ),
--- AppDependencies/Package_NoLicense.swift
+++ AppDependencies/Package_NoLicense.swift
@@ -29,6 +29,10 @@ let package = Package(
             url: "https://github.com/CombineCommunity/CombineExt",
             .upToNextMajor(from: "1.2.0")
         ),
+        .package(
+            url: "https://github.com/sparkle-project/Sparkle",
+            .branch("master")
+        ),
     ],
     targets: [
         .target(
@@ -37,6 +41,7 @@ let package = Package(
                 "CGEventOverride",
                 "CombineExt",
                 "KeychainAccess",
+                "Sparkle",
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
             ]
         ),
